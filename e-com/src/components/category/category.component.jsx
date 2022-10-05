0cdaccd1fc545a363822459cb6bbe4aa6cc01d0d
Nilansh Audichya <109611014+Nilansh1210@users.noreply.github.com>
2022-10-05 12:16:31 +0530

    Initial commit

---
+++ e-com/src/components/category/category.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CategoryItem from "../category-item/category-item.component.jsx";
+import './category.styles.scss'
+
+function CategoryCont({categories}) {
+    return (
+        <div className="categories-container">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category} />
+            ))}
+        </div>
+    )
+}
+
+export default CategoryCont;
